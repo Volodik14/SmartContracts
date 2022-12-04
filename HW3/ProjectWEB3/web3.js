7c76f256b00c30c10bb7e4ff841bbb85b39d23b9
Volodik14 <motorkin.v@gmail.com>
2022-12-04 22:55:34 +0300

    Добавил 3-е ДЗ, изменил структуру папок.

---
+++ HW3/ProjectWEB3/web3.js
@@ -0,0 +1,43 @@
+const Web3 = require('web3')
+var web3 = new Web3("https://mainnet.infura.io/v3/1fbfe6f0fae94281be2a3127259ea517");
+const address = "0xCf0054c729f41cA5b3508c2B243260664aFb20a9";
+
+// Смарт-контракт суммирует 2 числа и может возвращать результат суммирования
+const ABI = [
+      {
+        "inputs": [
+          {
+            "internalType": "int256",
+            "name": "value1",
+            "type": "int256"
+          },
+          {
+            "internalType": "int256",
+            "name": "value2",
+            "type": "int256"
+          }
+        ],
+        "name": "add",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "getSum",
+        "outputs": [
+          {
+            "internalType": "int256",
+            "name": "",
+            "type": "int256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ];
+
+web3.eth.getBalance ;
+
+const myContract = new web3.eth.Contract(ABI, address);
+myContract.methods.getSum().call().then(console.log);
