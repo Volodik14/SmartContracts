971f9480a4f1466277dcaf7465c1390fb780223f
Volodik14 <motorkin.v@gmail.com>
2022-12-20 05:18:34 +0300

    Updated HW3

--- HW3/ProjectWEB3/web3.js
+++ HW3/ProjectWEB3/web3.js
@@ -1,6 +1,6 @@
 const Web3 = require('web3')
-var web3 = new Web3("https://mainnet.infura.io/v3/1fbfe6f0fae94281be2a3127259ea517");
-const address = "0x43b1578e4cb49670ddc03237537ab2247388e01f";
+var web3 = new Web3("https://goerli.infura.io/v3/1fbfe6f0fae94281be2a3127259ea517");
+const address = "0x5039Aa3D412eF3B7fa2440211ae3dB5bfEFcbbFf";
 
 const ABI = [
       {
@@ -36,7 +36,7 @@ const ABI = [
       }
     ];
 
-web3.eth.getBalance ;
+web3.eth.getBalance;
 
 const myContract = new web3.eth.Contract(ABI, address);
 myContract.methods.getSum().call().then(console.log);
